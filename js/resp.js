934b164373699a5d6a583daa9bc77535e40d85c5
Shivam <shiv.ompr97@gmail.com>
2021-10-20 10:35:21 +0530

    second commit

--- js/resp.js
+++ js/resp.js
@@ -1,10 +1,10 @@
 burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
-nav-List = document.querySelector('.nav-list')
+navList = document.querySelector('.nav-list')
 rightNav = document.querySelector('.rightNav')
 
-burger.addEventlistner('click', ()=>{
+burger.addEventListner('click', ()=>{
     rightNav.classList.toggle('v-class');
     navList.classList.toggle('v-class');
-    navbar.classList.toggle('h-nav';)
+    navbar.classList.toggle('h-nav');                                                          
 })
